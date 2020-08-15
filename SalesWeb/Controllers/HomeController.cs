a0cdc8f728b1f8d8847eb50c667612e7a40e9995
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2020-08-15 18:51:20 -0300

    First Model-Controller-View - Department

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWeb.Models;
+using SalesWeb.Models.ViewModels;
 
 namespace SalesWeb.Controllers
 {
