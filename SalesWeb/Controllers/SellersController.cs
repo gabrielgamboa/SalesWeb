e3a97c689fbd3aecd46e4944725d77f34fed26ce
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2020-08-16 19:03:02 -0300

    Simple Create form

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Models;
 using SalesWeb.Services;
 
 namespace SalesWeb.Controllers
@@ -21,6 +22,21 @@ namespace SalesWeb.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
