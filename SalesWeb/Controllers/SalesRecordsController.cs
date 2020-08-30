9b36d099408c75b55451555351c5530078d4e4a1
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2020-08-30 19:38:32 -0300

    Implementing grouping search

--- SalesWeb/Controllers/SalesRecordsController.cs
+++ SalesWeb/Controllers/SalesRecordsController.cs
@@ -33,13 +33,26 @@ namespace SalesWebMvc.Controllers
             }
             ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
             ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
             var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
             return View(result);
         }
 
         public async Task<IActionResult> GroupingSearch (DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
--- SalesWeb/Models/SalesRecord.cs
+++ SalesWeb/Models/SalesRecord.cs
@@ -1,6 +1,7 @@
 ﻿using SalesWeb.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,13 @@ namespace SalesWeb.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
         public int MyProperty { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
