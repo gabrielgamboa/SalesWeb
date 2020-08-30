720d25a21525912262b8b7f357fd71c8fdbff433
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2020-08-30 19:22:53 -0300

    Implementing simple search

---
+++ SalesWeb/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Models;
+
+namespace SalesWeb.Services
+{
+    public class SalesRecordService : Controller
+    {
+        private readonly SalesWebContext _context;
+
+        public SalesRecordService (SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        } 
+    }
+}
--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -42,7 +42,7 @@ namespace SalesWeb.Services
                 _context.Seller.Remove(obj);
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateException e ) 
+            catch (DbUpdateException) 
             {
                 throw new IntegrityException("Can't delete seller because he/she has sales");
             }
